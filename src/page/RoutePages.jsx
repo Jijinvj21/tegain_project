09645d39dfb9a9f3ba96c6b6e0d8c81ff689f4f1
Jijin vj <jijinvjvazhippilly@gmail.com>
2024-04-18 09:46:05 +0530

    netlify configration and back button added

--- src/page/RoutePages.jsx
+++ src/page/RoutePages.jsx
@@ -1,10 +1,24 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function RoutePages({ page }) {
+  const navigate = useNavigate();
+
   useEffect(() => {
     localStorage.setItem("lastVisitedPath", page);
   });
-  return <h1>{page}</h1>;
+  return (
+    <div>
+      <h1>{page}</h1>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        Back
+      </button>
+    </div>
+  );
 }
 
 export default RoutePages;
